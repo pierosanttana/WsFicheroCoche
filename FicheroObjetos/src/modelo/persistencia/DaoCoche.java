521f38c9e535cc88efb403f004489e43b269c3ac
Guillemj98 <guillemagro98@gmail.com>
2024-10-23 12:35:06 +0200

    Add de todos los metodos y su documentacion de la parte de DAO

--- FicheroObjetos/src/modelo/persistencia/DaoCoche.java
+++ FicheroObjetos/src/modelo/persistencia/DaoCoche.java
@@ -13,6 +13,7 @@ import modelo.entidad.Coche;
 
 public class DaoCoche {
 	private static final String NOMBRE_FIHERO = "coches.dat";
+	private static final String FICHERO_TEMPORAL = "ficheroTemporal.dat";
 
 	/**
 	 * Metodo que dado un ID pasado por parametro busca su coincidencia en el
@@ -41,8 +42,9 @@ public class DaoCoche {
 	}
 
 	/**
-	 * 
-	 * @param c
+	 * Metodo que registra un coche pasado por parametro.Comprueba la existencia
+	 * del fichero en caso de que no exista devuelve una excepcion
+	 * @param c coche que pasamos por parametro
 	 * @throws Exception
 	 */
 	public void registrar(Coche c) throws Exception {
@@ -54,7 +56,7 @@ public class DaoCoche {
 		try (FileOutputStream fichero = new FileOutputStream(NOMBRE_FIHERO, true);
 				ObjectOutputStream escritor = new ObjectOutputStream(fichero);) {
 			escritor.writeObject(c);
-			System.out.println("El objeto ha sifo guardado correctamente");
+			System.out.println("El objeto ha sido guardado correctamente");
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -62,38 +64,74 @@ public class DaoCoche {
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Metodo que genera un ID y delvuelve ese ID
+	 * @return ID que devuelve
 	 * @throws Exception
 	 */
-	public long generadorId() throws Exception{
+	public long generadorId() throws Exception {
 
 		return 0;
 	}
+
 	/**
-	 * 
-	 * @return
+	 * Metodo que lee el fichero y lo almacena en un arrayList
+	 * @return devuelve un arrayList de coches
 	 * @throws Exception
 	 */
-	public ArrayList<Coche>getListaCoches() throws Exception{
-		ArrayList<Coche>listaCoches = null;;
-		
-		
-		
+	public ArrayList<Coche> getListaCoches() throws Exception {
+		ArrayList<Coche> listaCoches = null;
+		;
+		try (FileInputStream file = new FileInputStream(NOMBRE_FIHERO);
+				ObjectInputStream obj = new ObjectInputStream(file)) {
+			Coche coche = (Coche) obj.readObject();
+			System.out.println("El coche se ha deserializado");
+			listaCoches.add(coche);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return listaCoches;
-		
-		
-		
+
 	}
+
 	/**
-	 * 
+	 * Metodo que borra un coche mediante un ID pasado por parametro. Comprueba 
+	 * si el arrayLIst de coches esta vacio o es nulo, en caso afirmativo devulve una
+	 * excepcion.
+	 * Si el ID coincide lo borra y sobreescribe el array.
 	 * @param coche
-	 * @return
+	 * 
 	 * @throws Exception
 	 */
-	public boolean borrarCocheById(Coche coche)throws Exception {
-		
-		return false;
+	public void borrarCocheById(long id) throws Exception {
+		ArrayList<Coche>listaCoches = getListaCoches();
+		if(listaCoches.isEmpty() || listaCoches== null) {
+			// Devolvemos algo no se el que
+		}
+		boolean encontrado = false;
+		for(Coche coche :listaCoches) {
+			if(coche.getID() == id) {
+				encontrado = true;
+				break;
+			}
+		}
+		if(encontrado) {
+			ArrayList<Coche>listaCochesActualizado = new ArrayList<Coche>();
+			
+			for(Coche coche: listaCoches) {
+				if(coche.getID()!= id) {
+					listaCochesActualizado.add(coche);
+				}
+			}
+			try(FileOutputStream file1 = new FileOutputStream(NOMBRE_FIHERO);
+				ObjectOutputStream obj1 = new ObjectOutputStream(file1)){
+				for(Coche coche :listaCochesActualizado) {
+					obj1.writeObject(coche);
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
